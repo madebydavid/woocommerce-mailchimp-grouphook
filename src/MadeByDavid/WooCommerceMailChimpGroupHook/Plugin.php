1ec825f3fd4040b1901d888687baefaecbc6b5a2
madebydavid <sutherland.dave@gmail.com>
2014-03-03 19:15:37 +0000

    adding hook and filter

--- src/MadeByDavid/WooCommerceMailChimpGroupHook/Plugin.php
+++ src/MadeByDavid/WooCommerceMailChimpGroupHook/Plugin.php
@@ -11,14 +11,29 @@ class Plugin {
         
         add_action('init', array($this, 'init'), 0);
         
-
+        add_action('woocommerce_order_status_changed', array($this, 'orderStatusChanged'), 0, 3);
+        
+        add_filter('ss_wc_mailchimp_subscribe_merge_vars', array($this, 'determineMailchimpGroups'));
+        
     }
     
     public function init(){
         
     }
     
+    
+    public function orderStatusChanged($id, $status = 'new', $new_status = 'pending' ) {
        
+        error_log("orderStatusChanged");
+    }
+    
+    public function determineMailchimpGroups($mergeVars) {
+        
+        error_log("determineMailchimpGroups");
+        
+        return $mergeVars;
+    }
+    
     
 
 }
